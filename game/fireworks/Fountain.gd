c49ed214577a6bbfc6bb5e7ca757dd725e05a7d8
hinek <der@hinek.de>
2020-12-29 22:39:08 +0100

    Better looking start and end of fountains

--- game/fireworks/Fountain.gd
+++ game/fireworks/Fountain.gd
@@ -2,6 +2,7 @@ extends Node2D
 
 
 var lifetime = 10
+var target_velocity = 200
 
 
 onready var particles = $Particles
@@ -12,12 +13,19 @@ func _ready():
 	particles.color_ramp = load(str("res://colors/" + str(color) + ".tres"))
 	particles.hue_variation = 0.3
 	particles.hue_variation_random = 0.5
+	particles.initial_velocity = target_velocity / 10
 	$Timer.start(lifetime)
+
+
+func _process(delta):
+	particles.initial_velocity = lerp(particles.initial_velocity, target_velocity, delta)
+	if particles.initial_velocity < 10:
+		particles.emitting = false
+	if particles.initial_velocity < 1:
+		queue_free()
 
 
 func _on_Timer_timeout():
 	if particles.is_emitting():
-		particles.emitting = false
+		target_velocity = 0
 		$Timer.start(particles.lifetime)
-	else:
-		queue_free()
